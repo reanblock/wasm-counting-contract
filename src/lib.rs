8b8e44bc4557bed3ede0948f0faf40013b439dfa
Darren Jensen <darren.jensen@gmail.com>
2024-06-01 11:21:14 +0700

    added msg module

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 use cosmwasm_std::{
-    entry_point, Binary, Deps, DepsMut, Empty, Env, MessageInfo, Response, StdResult,
+    entry_point, to_json_binary, Binary, Deps, DepsMut, Empty, Env, MessageInfo, Response, StdResult,
 };
+
+mod contract;
+pub mod msg;
  
 #[entry_point]
 pub fn instantiate(_deps: DepsMut, _env: Env, _info: MessageInfo, _msg: Empty,
@@ -14,8 +17,14 @@ pub fn execute(_deps: DepsMut, _env: Env, _info: MessageInfo, _msg: Empty) -> St
 }
 
 #[entry_point]
-pub fn query(_deps: Deps, _env: Env, _msg: Empty) -> StdResult<Binary> {
-    Ok(Binary::default())
+pub fn query(_deps: Deps, _env: Env, msg: msg::QueryMsg) -> StdResult<Binary> {
+    use msg::QueryMsg::*;
+    use contract::query;
+ 
+    match msg {
+        Value {} => to_json_binary(&query::value()),
+        Incremented { value } => to_json_binary(&query::incremented(value)),
+    }
 }
 
 pub fn add(left: usize, right: usize) -> usize {
